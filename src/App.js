715d8a33d5857b74e5008ed445bd7ed07c4a7733
sathishlk <105412258+sathishlk@users.noreply.github.com>
2022-05-15 18:14:44 +0530

    Add files via upload
    new commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    
+      <div>
+    <table> 
+      <h1>saf</h1>
+      <h2>oiutssrdfgiuyfghjkjh</h2>
+    
+
+
+
+    </table>
+
+    </div>
+  );
+}
+
+export default App;
